4f96ad309f0316ce10b89f0702c5a4517c2fdb44
GustavoPisanello <gustavo.pisanello@gmail.com>
2024-10-31 11:30:25 -0300

    MovieDetails page finished

---
+++ portal-filmes/src/components/cast/CastCard.jsx
@@ -0,0 +1,15 @@
+export default function CastCard({ name, role, character, profile_path }) {
+    return (
+        <div className="flex flex-col cast-card gap-y-2">
+            <div>
+                {profile_path ? (<img src={`https://image.tmdb.org/t/p/w200${profile_path}`} alt={name} />) :
+                ("")
+            }
+            </div>
+            <div className="text-center">
+                <h3 className="text-sm">{name}</h3>
+                {role === "Acting" && <p className="text-xs text-primary_color">{character}</p>}
+            </div>
+        </div>
+    );
+}
--- portal-filmes/src/pages/MovieDetails.jsx
+++ portal-filmes/src/pages/MovieDetails.jsx
@@ -1,39 +1,67 @@
 import {useParams} from 'react-router-dom'
-import {useEffect, useState} from 'react'
+import {useEffect, useState, useRef} from 'react'
 import { CiStar } from "react-icons/ci";
+import { IoIosArrowDown } from "react-icons/io";
+import { IoIosArrowUp } from "react-icons/io";
+import CastCard from '../components/cast/CastCard';
 
 export default function MovieDetails(){
 
-    const {id} = useParams()
+    const {id} = useParams();
     const [movie, setMovie] = useState({});
-    const [trailer, setTrailer] = useState({});
+    const [cast, setCast] = useState([]);
+    const [trailer, setTrailer] = useState(null);
 
 
     useEffect(() => {
         fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=114e0cc10385f69dc6d21e2a19caf57a&language=pt-br`)
         .then(response => response.json())
         .then(data => {setMovie(data)})
-        .catch(e => console.error(e))
+        .catch(e => console.error(e));
     }, []);
 
     useEffect(() => {
-        fetch(`https://api.themoviedb.org/3/movie/${id}/videos?api_key=114e0cc10385f69dc6d21e2a19caf57a&language=pt-br`)
-        .then(response => response.json())
-        .then(data => {
-            if (data.results.lenght > 0){
-                const trailerURL = data.results[0].key;
-                setTrailer(`https://www.youtube.com/watch?v=${trailerURL}`);
+        fetch(
+          `https://api.themoviedb.org/3/movie/${id}/videos?api_key=114e0cc10385f69dc6d21e2a19caf57a&language=pt-br`
+        )
+          .then((response) => response.json())
+          .then((data) => {
+            if (data.results.length > 0) {
+              const trailerURL = data.results[0].key;
+              setTrailer(`https://www.youtube.com/watch?v=${trailerURL}`);
             }
-        })
-        .catch(e => console.error(e));
-    }, [id]);
+          })
+          .catch((err) => console.error(err));
+      }, [id]);
 
+      useEffect(() => {
+        fetch(
+            `https://api.themoviedb.org/3/movie/${id}/credits?api_key=114e0cc10385f69dc6d21e2a19caf57a&language=pt-br&page=1`
+        )
+        .then((response) => response.json())
+        .then((data) => setCast(data.cast))
+        .catch(e => console.error(e))
 
+        console.log(cast)
+      }, [id])
+
+    const carouselRef = useRef(null);
+
+    const downScroll = () => {
+        carouselRef.current.scrollBy({top: 200, behavior: 'smooth'})
+        console.log('Scroll Height:', carouselRef.current.scrollHeight);
+        console.log('Client Height:', carouselRef.current.clientHeight);
+    }
+
+    const upScroll = () => {
+        carouselRef.current.scrollBy({top: -200, behavior: 'smooth'})
+    }
+    
     return (
         <> 
         {
             movie ?
-                <div key={movie.id} className='h-[1000000px] bg-cover bg-center text-white relative' style={{backgroundImage: `url(https://image.tmdb.org/t/p/original${movie.backdrop_path})`, maxHeight: "calc(100vh - 80.8px)"}}>
+                <div key={movie.id} className='h-[800px] bg-cover bg-center text-white relative' style={{backgroundImage: `url(https://image.tmdb.org/t/p/original${movie.backdrop_path})`}}>
                     <div className="bg-black bg-opacity-70 w-full h-full absolute top-0 left-0 flex">
                         <div className="w-[60%] p-20 h-full font-[Raleway] flex flex-col gap-y-8">
                             <div className="w-full flex">
@@ -49,6 +77,47 @@ export default function MovieDetails(){
                                 </div>
                             </div>
                             <div className='text-justify'>{movie.overview}</div>
+                            <div className='flex justify-between'>
+                                <div className="w-[75%]">
+                                {trailer ? (
+                                    <iframe
+                                    width="100%"
+                                    height="315"
+                                    src={trailer.replace('watch?v=', 'embed/')}
+                                    title={`${movie.title} Trailer`}
+                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                    allowFullScreen
+                                  />
+                                    ) : (
+                                    <p>Trailer não disponível</p>
+                                    )}
+                                </div>
+                                <div className="w-[20%] flex justify-center">
+                                    <div className="w-[90%] h-full relative">
+                                        <button onClick={upScroll} className=" text-black flex items-center w-6 absolute left-[37%] -top-4 rounded-full bg-white text-4xl z-10" >
+                                            <IoIosArrowUp />
+                                        </button>
+
+                                        <div ref={carouselRef} className="flex flex-col gap-y-8 overflow-hidden max-h-[315px]">
+                                            {
+                                                cast.map((data, index) => (
+                                                    <CastCard
+                                                        key = {index}
+                                                        name = {data.name}
+                                                        role = {data.known_for_department}   
+                                                        character={data.character}  
+                                                        profile_path = {data.profile_path}                                               
+                                                    />
+                                                ))
+                                            }
+                                        </div>
+                                        
+                                        <button onClick={downScroll} className=" text-black flex items-center w-6 absolute left-[37%] -bottom-4 rounded-full bg-white text-4xl z-10" >
+                                            <IoIosArrowDown />
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                         <div className="w-[40%] h-full">
                             <div className="w-[90%] h-full">
